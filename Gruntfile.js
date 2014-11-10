5247d802db7b0aed2d07797528a8baabe4b0ed1a
John Foster <john@varyn.com>
2014-11-10 16:23:51 -0500

    update grunt, add clean, fix package, update distrib

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,8 @@
-/* Gruntfile for MatchMaster HTML5 project production packging.
+/* Gruntfile for Top Chef Memory Challenge HTML5 project production packging.
  *
  *         Project Dependencies:
  npm install grunt --save-dev
+ npm install grunt-contrib-clean --save-dev
  npm install grunt-contrib-watch --save-dev
  npm install grunt-contrib-imagemin --save-dev
  npm install grunt-contrib-htmlmin --save-dev
@@ -9,6 +10,14 @@
  npm install time-grunt --save-dev
  npm install grunt-exec --save-dev
 
+ * Top Chef Memory Challenge workflow:
+ *   (if release) clean the distrib folder
+ *   copyin from Enginesis game work area to source folder
+ *   (if release) copy the source folder to the distrib folder
+ *   concat + uglify the JS
+ *   (if release) imagemin the images into the distrib folder
+ *   copy the minified source files from distrib folder to game work area
+ *   (if release) build distribution package
  */
 
 module.exports = function(grunt) {
@@ -77,37 +86,56 @@ module.exports = function(grunt) {
             tasks: 'default'
         },
 
-        /* Copy the minified files to the destination folders so we stay in-sync */
+        /* Clean out the distrib folder so we make sure we get a clean copy of everything */
+        clean: {
+			release: ["distrib/js", "distrib/lib", "distrib/assets", "distrib/images", "distrib/*.html", "distrib/*.appcache"]
+		},
+
         exec: {
+        	/* Make a distribution package */
+			package: {
+				command: 'sh ./package.sh',
+				stdout: true,
+				stderr: true
+			},
+        	/* Copy files from the development area into the build area */
 			copyin: {
 				command: 'sh ./copyin.sh',
 				stdout: true,
 				stderr: true
 			},
+			/* Copy the build files into the distribution holding area */
+            copydistrib: {
+                command: 'rsync -av source/ distrib',
+                stdout: true,
+                stderr: true
+            },
+            /* After minify, copy the minified game files to the destination folders so we stay in-sync */
             copymin: {
                 command: 'cp distrib/js/MemoryMatch.min.js source/js/MemoryMatch.min.js && cp distrib/js/MemoryMatch.min.js ../../websites/enginesis/public/games/TopChefMemoryChallenge/js/MemoryMatch.min.js',
                 stdout: true,
                 stderr: true
             },
+            /* After minify, copy the minified library files to the destination folders so we stay in-sync */
             copyenginesismin: {
                 command: 'cp distrib/lib/enginesis.min.js source/lib/enginesis.min.js && cp distrib/lib/enginesis.min.js ../../websites/enginesis/public/games/TopChefMemoryChallenge/lib/enginesis.min.js',
                 stdout: true,
                 stderr: true
             }
         },
-
-        /* Copy the project files from the test area into the source folder */
     });
 
     // Tell Grunt we plan to use these plug-ins:
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-exec');
 
-    // Tell Grunt what to do when we type "grunt" into the terminal.
+    // Run grunt default (or just grunt) to update the build area and minify the code for release testing
+    // Run grunt release to update the build area, minify, optimize images, and package a distribution
     grunt.registerTask('default', ['exec:copyin', 'concat', 'uglify', 'exec:copymin', 'exec:copyenginesismin']);
-    grunt.registerTask('release', ['exec:copyin', 'concat', 'uglify', 'imagemin', 'exec:copymin', 'exec:copyenginesismin']);
+    grunt.registerTask('release', ['clean:release', 'exec:copyin', 'exec:copydistrib', 'concat', 'uglify', 'imagemin', 'exec:copymin', 'exec:copyenginesismin', 'exec:package']);
 
 };
